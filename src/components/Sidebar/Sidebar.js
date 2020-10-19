2537b61a17554737e6cc0cbc2346bb46399fac7d
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-10-19 18:19:20 -0300

    login and logout functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,12 +4,17 @@ import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import SidebarChat from "./SidebarChat/SidebarChat";
+import {  useSelector } from "react-redux";
+import { logout, selectUser } from "../../features/userSlice";
+import { auth } from "../../firebase";
 
 function Sidebar() {
+  const user=useSelector(selectUser)
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src="" className="sidebar__avatar" />
+        <Avatar onClick={()=>auth.signOut()} src={user?.photo} className="sidebar__avatar" />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
